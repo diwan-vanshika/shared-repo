2320de46b4ce763eaedef601c6b799febb31bf49
Vanshika Diwan <95582026+diwan-vanshika@users.noreply.github.com>
2022-10-14 16:27:19 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,16 @@
+
+public class prog1  extends prog1_abs{
+	
+	void give_output()
+	{
+		System.out.println("You made an abstract class");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Class 1 created");
+		prog1 obj =new prog1();
+		obj.give_output();
+
+	}
+
+}
